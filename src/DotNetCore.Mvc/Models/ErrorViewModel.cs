884066f1f71c750430b70866eba642fde5123033
developerazharshaikh <developerazharshaikh@gmail.com>
2022-09-14 21:18:07 +0530

    Added new solution with mvc core project

---
+++ src/DotNetCore.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCore.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
